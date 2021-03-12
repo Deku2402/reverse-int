79287f6cf95e494c1f8471b0175a8de97e451365
Deku2402 <gonfreeks8@gmail.com>
2021-03-12 22:24:03 +0600

    feat: create code for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let revers = 0;
+    if (n < 0) {
+        n = n * (-1);
+    }
+    while (n > 0) {
+        revers = revers * 10 + n % 10;
+        n = Math.floor(n / 10);
+
+    }
+    return revers;
 }
